fa91650131710666dd48cb1caafdd89f42b2bde6
hooper <2020104216@qq.com>
2022-04-27 22:33:45 +0800

    new file:   "1.0.0enum\346\236\232\344\270\276.cpp"

---
+++ CHIGH/1.0.0enum枚举.cpp
@@ -0,0 +1,11 @@
+//#include<stdio.h>
+//void main() {
+//	enum DAY {
+//		MON=1,TUE=2,WED=3,THU=4,FRI=5,SAT=6,SUN=7
+//	};//这里的DAY就是枚举类型，包含了7个枚举元素
+//	enum DAY day;//enum DAY 是枚举类型，day是枚举变量（可以联系 int a ; int 是数据类型，a是变量）
+//	day = WED;//给枚举变量赋值，值就是某个枚举元素
+//	printf("星期%d", day);//输出为3，每个枚举元素对应一个值
+//	getchar();
+//	return ;
+//}

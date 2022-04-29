02111cfd25c859d1e1c634a69e961442bb8f1343
hooper <2020104216@qq.com>
2022-04-29 21:40:02 +0800

    myfun.c

---
+++ CHIGH/myfun.c
@@ -0,0 +1,26 @@
+////实现 int myCal(int n1,int n2,char oper)
+////定义函数将会被头文件myfun.c调用
+//#include<stdio.h>
+//int myCal(int n1, int n2, char oper) {
+//	double res = 0.0;
+//	
+//		switch (oper) {
+//		case'+':
+//			res = n1 + n2;
+//			break;
+//		case'-':
+//			res = n1 - n2;
+//			break;
+//		case '*':
+//			res = n1 * n2;
+//			break;
+//		case '/':
+//			res = n1 / n2;
+//			break;
+//		default:
+//			printf("输入有误");
+//			break;
+//		}printf("%d %c %d = %.2f", n1, oper, n2, res);
+//		return res;
+//	
+//}

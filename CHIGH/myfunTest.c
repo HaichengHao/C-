372a1335c9623ab988b4ddfdf94b6d9a726bdad0
hooper <2020104216@qq.com>
2022-04-29 21:41:48 +0800

    myfunTest.c

---
+++ CHIGH/myfunTest.c
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+//#include"myfun1.h"//调用头文件myfun1.h,只有调用它，才能用它里边的函数
+//void main() {
+//	int n1 = 10;
+//	int n2 = 10;
+//	char oper1 = '+';
+//	double res = 0.0;
+//
+//	//调用myfun1.c中的myCal函数
+//	res = myCal(n1, n2, oper1);
+//	printf("\nres = %.2f", res);
+//}

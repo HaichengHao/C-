8a593abe6a8b0a6e602540d7cdeb4d14ea007fd2
hooper <2020104216@qq.com>
2022-04-30 20:45:15 +0800

    2.4varInit.cpp

---
+++ CHIGH/2.4varInit.cpp
@@ -0,0 +1,15 @@
+////#include<stdio.h>
+////void main() {
+////	int i;
+////	printf("%d",i);//系统不会对局部变量默认初始化，必须对局部变量初始化后才能使用
+////}
+//
+//#include<stdio.h>
+////全局变量时会默认初始化的
+//int i;
+//float  f;
+//double d1;
+//void main() {
+//	printf("\n i =%d , f = %f, d1 = %f", i, f, d1);
+//	// i =0 , f = 0.000000, d1 = 0.000000
+//}

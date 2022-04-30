1d12daf67a915b3cd9f570b0ef517cdb18e1199e
hooper <2020104216@qq.com>
2022-04-30 16:49:48 +0800

    2.1.0函数递归recursion.cpp

---
+++ CHIGH/2.1.0函数递归recursion.cpp
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//void test(int n) {
+//	if (n > 2) {
+//		test(n - 1);
+//	}
+//	else {
+//		printf("n=%d\n", n);
+//	}
+//}
+//void main() {
+//	int n1 = 4;
+//	test(n1);
+//}
+///*
+//n=2
+//n=3
+//n=4
+//*/

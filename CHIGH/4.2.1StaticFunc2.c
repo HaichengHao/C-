52123937ed7b5f5e13bbf05fdb4c986693602047
hooper <2020104216@qq.com>
2022-05-01 09:28:07 +0800

    4.2.1StaticFunc2.c

--- CHIGH/4.2.1StaticFunc2.c
+++ CHIGH/4.2.1StaticFunc2.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-extern void func1();//引用非静态函数，只需要extern +返回值类型，函数名，参数列表即可
-void main() {
-	int a = 20;
-	func1(a);
-}
+//#include<stdio.h>
+//extern void func1();//引用非静态函数，只需要extern +返回值类型，函数名，参数列表即可
+//void main() {
+//	int a = 20;
+//	func1(a);
+//}

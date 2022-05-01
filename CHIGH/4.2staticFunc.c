b567b1491bf0ca2a90805a8636aeebacfe982aba
hooper <2020104216@qq.com>
2022-05-01 09:27:49 +0800

    4.2staticFunc.c

--- CHIGH/4.2staticFunc.c
+++ CHIGH/4.2staticFunc.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-void func1(int a) {//普通函数，非静态函数
-	printf("hello from func1\n");
-	printf("a=%d", a);
-}
-static void func2(void) {//静态函数，只可在当前文件使用
-	printf("hello from fun2\n");
-}
+//#include<stdio.h>
+//void func1(int a) {//普通函数，非静态函数
+//	printf("hello from func1\n");
+//	printf("a=%d", a);
+//}
+//static void func2(void) {//静态函数，只可在当前文件使用
+//	printf("hello from fun2\n");
+//}
